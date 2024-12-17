4c4bf6eb7128b56767b44bd1a5e99cf450aee5dc
Gustavo Espitia <146354509+gusespitia@users.noreply.github.com>
2024-12-17 13:25:33 +0100

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,21 @@
 import { defineConfig, envField } from 'astro/config';
 
 
+import vercel from '@astrojs/vercel';
+
+
 // https://astro.build/config
 // estos nos permiten acceder a las variables de entorno del servidor
 export default defineConfig({
-    output: 'server',
-    env: {
-        schema:
-        {
-            SHOW_BUY_BUTTON: envField.boolean({default: true, context: 'server', access:'public'}),
-            SCORE_API_ENDPOINT: envField.string({context: 'server', access:'public' })
-        }
-    }
+  output: 'server',
+
+  env: {
+      schema:
+      {
+          SHOW_BUY_BUTTON: envField.boolean({default: true, context: 'server', access:'public'}),
+          SCORE_API_ENDPOINT: envField.string({context: 'server', access:'public' })
+      }
+  },
+
+  adapter: vercel()
 });
